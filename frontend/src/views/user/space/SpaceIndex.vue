04cf327ac983097b183f4f38e9217fa542e84dd6
HHH001 <h2392380970@163.com>
2026-02-04 21:49:35 +0800

    实现注册，登录页面

---
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute(); // 获取当前路由对象(可以操作当前路由的参数，比如下面的取出user_id)
+</script>
+
+<template>
+个人空间:{{ route.params.user_id }}
+</template>
+
+<style scoped>
+
+</style>
