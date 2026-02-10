009e2a4c913d2d52f7b478738e1e7cb8b47f26b4
HHH001 <h2392380970@163.com>
2026-02-11 00:00:07 +0800

    实现角色增删改查后端api

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from web.models.user import UserProfile
-
+from web.models.character import Character
 
 @admin.register(UserProfile)
 class UserAdmin(admin.ModelAdmin):
@@ -8,3 +8,7 @@ class UserAdmin(admin.ModelAdmin):
     # ORM 再通过 settings.py 的 DATABASES 配置连接到 sqlite3。
     # 最终 SQL 由 Django 生成并执行，数据写入 db.sqlite3 文件。
     raw_id_fields = ('user',)  # 外键/一对一字段用输入框，避免下拉加载过多数据。
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ('author',)
